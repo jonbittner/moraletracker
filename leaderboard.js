d1b1cbb99cad904adb5976be37f6587f3896e506
Jonathan Bittner <jonbittner@ubuntu.(none)>
2012-10-28 12:08:02 -0700

    pictures fixed,ish

--- leaderboard.js
+++ leaderboard.js
@@ -44,13 +44,13 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     if (Players.find().count() === 0) {
-	Players.insert({name: "Jon", avatar: "http://gravatar.com/HASH/25e18cffe259859098f45d213b2f6db4",});
-	Players.insert({name: "Ryan", avatar: "http://gravatar.com/HASH/25e18cffe259859098f45d213b2f6db4",});
-	Players.insert({name: "Marshall", avatar: "http://gravatar.com/HASH/25e18cffe259859098f45d213b2f6db4",});    
-	Players.insert({name: "Nellie", avatar: "http://gravatar.com/HASH/25e18cffe259859098f45d213b2f6db4",});    
-	Players.insert({name: "Caleb", avatar: "http://gravatar.com/HASH/25e18cffe259859098f45d213b2f6db4",});    
-	Players.insert({name: "Kyle", avatar: "http://gravatar.com/HASH/25e18cffe259859098f45d213b2f6db4",});    
-	Players.insert({name: "Mark", avatar: "http://gravatar.com/HASH/25e18cffe259859098f45d213b2f6db4",});    
+	Players.insert({name: "Jon", avatar: "http://gravatar.com/avatar/25e18cffe259859098f45d213b2f6db4",});
+	Players.insert({name: "Ryan", avatar: "http://gravatar.com/avatar/b6d3539d5ffda82fd2c26d0fc42a126f",});
+	Players.insert({name: "Marshall", avatar: "http://a0.twimg.com/profile_images/1406604829/photo_4_reasonably_small.jpg",});    
+	Players.insert({name: "Nellie", avatar: "/public/nellie.jpg",});    
+	Players.insert({name: "Caleb", avatar: "http://a0.twimg.com/profile_images/2228011122/superpowers_reasonably_small.jpg",});    
+	Players.insert({name: "Kyle", avatar: "http://a0.twimg.com/profile_images/1754875012/384019_2422660721211_1092240058_32286172_1970484507_n_reasonably_small.jpg",});    
+	Players.insert({name: "Mark", avatar: "/public/mark.png",});    
     }
   });
 }
