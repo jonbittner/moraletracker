78a9719ad118e297ea0b0d5e9bfb6ed5af2d013b
Ubuntu64 <kyle@ubuntu.(none)>
2012-10-28 17:08:08 -0400

    more more more

--- leaderboard.js
+++ leaderboard.js
@@ -5,10 +5,36 @@ Players = new Meteor.Collection("players");
 
 if (Meteor.isClient) {
   Template.leaderboard.players = function () {
-    return Players.find({}, {sort: {score: -1, name: 1}});
+    return Players.find({}, {sort: {name: 1}});
   };
 
+  Template.player.done = function(id, elem) {
+    Players.update(id, {$set: {status: elem.find(".new_status").val()}});
+    elem.find(".status").show()
+    elem.find(".edit_status").hide()
+  }
+
   Template.player.events({
+    'click .one-star': function () {
+      Players.update(this._id, {$set: {score: 1}});
+    },
+
+    'click .two-stars': function () {
+      Players.update(this._id, {$set: {score: 2}});
+    },
+
+    'click .three-stars': function () {
+      Players.update(this._id, {$set: {score: 3}});
+    },
+
+    'click .four-stars': function () {
+      Players.update(this._id, {$set: {score: 4}});
+    },
+
+    'click .five-stars': function () {
+      Players.update(this._id, {$set: {score: 5}});
+    },
+
     'click .inc': function () {
       Players.update(this._id, {$inc: {score: 1}});
     },
@@ -17,18 +43,29 @@ if (Meteor.isClient) {
       Players.update(this._id, {$inc: {score: -1}});
     },
 
-    'click a.edit': function (event) {
-      var player = $(event.target).closest(".player")
-      player.find(".status").hide()
-      player.find(".edit_status").show()
+    'click .status': function (event) {
+      var elem = $(event.target).closest(".player")
+      elem.find(".status").hide()
+      elem.find(".edit_status").show()
+      elem.find(".new_status").focus()
+
+      id = this._id
+
+      elem.find(".new_status").keydown( function(e) {
+        var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
+        if(key == 13) {
+          Template.player.done(id, elem);
+        }
+      })
+
+      elem.find(".new_status").focusout( function() {
+        Template.player.done(id, elem);
+      })
     },
 
     'click a.done': function (event) {
-      var player = $(event.target).closest(".player")
-      Players.update(this._id, {$set: {status: player.find(".new_status").val()}});
-      
-      player.find(".status").show()
-      player.find(".edit_status").hide()
+      var elem = $(event.target).closest(".player")
+      Template.player.done(this._id, elem);
     },
 
     'click a.cancel': function (event) {
@@ -44,13 +81,13 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     if (Players.find().count() === 0) {
-	Players.insert({name: "Jon", avatar: "http://gravatar.com/HASH/25e18cffe259859098f45d213b2f6db4",});
-	Players.insert({name: "Ryan", avatar: "http://gravatar.com/HASH/25e18cffe259859098f45d213b2f6db4",});
-	Players.insert({name: "Marshall", avatar: "http://gravatar.com/HASH/25e18cffe259859098f45d213b2f6db4",});    
-	Players.insert({name: "Nellie", avatar: "http://gravatar.com/HASH/25e18cffe259859098f45d213b2f6db4",});    
-	Players.insert({name: "Caleb", avatar: "http://gravatar.com/HASH/25e18cffe259859098f45d213b2f6db4",});    
-	Players.insert({name: "Kyle", avatar: "http://gravatar.com/HASH/25e18cffe259859098f45d213b2f6db4",});    
-	Players.insert({name: "Mark", avatar: "http://gravatar.com/HASH/25e18cffe259859098f45d213b2f6db4",});    
+      Players.insert({name: "Jon", avatar: "http://gravatar.com/avatar/25e18cffe259859098f45d213b2f6db4",});
+      Players.insert({name: "Ryan", avatar: "http://gravatar.com/avatar/25e18cffe259859098f45d213b2f6db4",});
+      Players.insert({name: "Marshall", avatar: "http://gravatar.com/avatar/25e18cffe259859098f45d213b2f6db4",});    
+      Players.insert({name: "Nellie", avatar: "http://gravatar.com/avatar/25e18cffe259859098f45d213b2f6db4",});    
+      Players.insert({name: "Caleb", avatar: "http://gravatar.com/avatar/25e18cffe259859098f45d213b2f6db4",});    
+      Players.insert({name: "Kyle", avatar: "http://gravatar.com/avatar/25e18cffe259859098f45d213b2f6db4",});    
+      Players.insert({name: "Mark", avatar: "http://gravatar.com/avatar/25e18cffe259859098f45d213b2f6db4",});    
     }
   });
 }
